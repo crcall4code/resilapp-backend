2bb275585800933741612f06f857c3aafbcd145e
Costa Rica Call4Code <crcall4code@gmail.com>
2019-07-09 00:25:30 +0000

    Added database functions to get towns, cities (cantons) and states (provinces), and set communities.

--- SQL_Database.py
+++ SQL_Database.py
@@ -1,68 +1,315 @@
-from sqlalchemy import Text, Float, MetaData, create_engine, Table, Column, insert, select
+from sqlalchemy import Text, Float, Integer, MetaData, create_engine, Table, Column, insert, select, and_, distinct, update
+from json import dumps
+from pprint import pprint
 
-class DB2:
+class DB2_Towns:
+    __instance = None
+    @staticmethod
+    def getInstance():
+        if DB2_Towns.__instance == None:
+            DB2_Towns()
+        return DB2_Towns.__instance
     def __init__(self):
-        self.username = "lfj47179"
-        self.password = "3hb62wh+7jtbb77n"
-        self.host = "dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net"
-        self.port = 50000
-        self.schema = "BLUDB"    #Schema:"LFJ47179"
-        self.engine = create_engine("db2+ibm_db://{}:{}@{}:{}/{}".format(self.username, self.password, self.host, self.port, self.schema))
-        self.mainConnection = self.engine.connect()
-        self.metadata = MetaData()
-        self.Communities = Table('COMMUNITIES', self.metadata,
-                                Column('NAME', Text(), nullable=False),
-                                Column('UBICATION_X', Float(), nullable=False),
-                                Column('UBICATION_Y', Float(), nullable=False),
-                                Column('PROVINCE', Text(), nullable=False),
-                                Column('CANTON', Text(), nullable=False),
-                                Column('RESILIENCE', Text(), nullable=False)
-                                )
+        if DB2_Towns.__instance != None:
+            raise Exception("This class is a singleton")
+        else:
+            DB2_Towns.__instance = self
+            self.username = "lfj47179"
+            self.password = "3hb62wh+7jtbb77n"
+            self.host = "dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net"
+            self.port = 50000
+            self.schema = "BLUDB"#"LFJ47179"
+            self.db2ConnectionString = "db2+ibm_db://{}:{}@{}:{}/{}".format(self.username, self.password, self.host, self.port, self.schema)
+            self.engine = create_engine(self.db2ConnectionString,pool_size=1, max_overflow=0)
+            self.mainConnection = self.engine.connect()
+            self.metadata = MetaData()
+            self.Towns = Table('POBLADOS', self.metadata,
+                                    Column('OBJECTID', Integer(), nullable=False, unique=True, primary_key=True),
+                                    Column('POBLAC_ID', Integer(), nullable=False),
+                                    Column('PROVINCIA', Text(), nullable=False),
+                                    Column('CANTON', Text(), nullable=False),
+                                    Column('PUEBLO', Text(), nullable=False),
+                                    Column('X_COORDENATES', Float(), nullable=False),
+                                    Column('Y_COORDENATES', Float(), nullable=False)
+                                    )
 
 
-    def insert(self, DataAsDictionary):
-        insert = self.Communities.insert().values(
-            NAME =DataAsDictionary["NAME"],
-            UBICATION_X =DataAsDictionary["UBICATION_X"],
-            UBICATION_Y =DataAsDictionary["UBICATION_Y"],
-            PROVINCE =DataAsDictionary["PROVINCE"],
-            CANTON =DataAsDictionary["CANTON"],
-            RESILIENCE=DataAsDictionary["RESILIENCE"]
-        )
-        insert.compile().params
-        result = self.mainConnection.execute(insert)
-
-    def select(self):
-        selectQuery = select([self.Communities]).where(self.Communities.c.NAME!=None)
+    def select_all_Towns(self):
+        selectQuery = select([self.Towns])
         resultsProxy = self.mainConnection.execute(selectQuery)
-        resultsList = []
+        TownsNames = []
         results = resultsProxy.fetchall()
         for row in results:
-            community = []
-            community.append(row[0]) #Name
-            community.append(row[1]) #Ubication_X
-            community.append(row[2]) #Ubication_Y
-            community.append(row[3]) #Province
-            community.append(row[4]) #Canton
-            community.append(row[5]) #Resilience
-            resultsList.append(community)
+            Town_City_State = "({},{},{})".format(row[4],row[3],row[2])
+            TownsNames.append(Town_City_State)
         resultsProxy.close()
-        return resultsList
+        return TownsNames
+
+
+    def select_all_States(self):
+        selectQuery = select([self.Towns.c.PROVINCIA]).distinct()
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        States = resultsProxy.fetchall()
+        States = [state[0] for state in States]
+        resultsProxy.close()
+        return States
+
+
+    def select_all_Cities_by_State(self, State):
+        selectQuery = select([self.Towns.c.CANTON]).distinct().where(self.Towns.c.PROVINCIA==State)
+        print(selectQuery)
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        Cities = resultsProxy.fetchall()
+        Cities = [city[0] for city in Cities]
+        resultsProxy.close()
+        return Cities
+
+
+    def select_all_Towns_by_State_and_City(self, State, City):
+        selectQuery = select([self.Towns.c.PUEBLO]).where(
+                                                            and_(
+                                                                self.Towns.c.PROVINCIA==State,
+                                                                self.Towns.c.CANTON==City
+                                                                )
+                                                            )
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        Towns = resultsProxy.fetchall()
+        Towns = [town[0] for town in Towns]
+        resultsProxy.close()
+        return Towns
+
+
+    def select_Towns_dictionaries_by_State(self, State):
+        selectQuery = select([self.Towns]).where(self.Towns.c.PROVINCIA==State)
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        Towns = resultsProxy.fetchall()
+        TownsList = []
+        for Town in Towns:
+            TownAsDictionary = dict(
+                CANTON=Town[3],
+                PUEBLO=Town[4]
+                )
+            TownsList.append(TownAsDictionary)
+        resultsProxy.close()
+        return TownsList
+
+
+    def select_Towns_dictionaries_by_State_and_City(self, State, City):
+        selectQuery = select([self.Towns]).where(
+                        and_(
+                            self.Towns.c.PROVINCIA==State,
+                            self.Towns.c.CANTON==City
+                            )
+                        )
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        Towns = resultsProxy.fetchall()
+        TownsList = []
+        for Town in Towns:
+            TownAsDictionary = dict(
+                PUEBLO=Town[4]
+                )
+            TownsList.append(TownAsDictionary)
+        resultsProxy.close()
+        return TownsList
+
+
+    def select_Town_dictionary_by_State_City_and_Name(self, State, City, Town):
+        selectQuery = select([self.Towns]).where(
+                        and_(
+                            self.Towns.c.PROVINCIA==State,
+                            self.Towns.c.CANTON==City,
+                            self.Towns.c.PUEBLO==Town
+                            )
+                        )
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        Town = resultsProxy.fetchone()
+        if Town!= None:
+            TownAsDictionary = dict(
+                ID=Town[0],
+                POBLAC_ID=Town[1],
+                PROVINCIA=Town[2],
+                CANTON=Town[3],
+                PUEBLO=Town[4],
+                X_COORDENATES=Town[5],
+                Y_COORDENATES=Town[6]
+                )
+        else:
+            TownAsDictionary=dict(Data="Town not found.")
+        resultsProxy.close()
+        return TownAsDictionary
+
+class DB2_Communities:
+    __instance = None
+    @staticmethod
+    def getInstance():
+        if DB2_Communities.__instance == None:
+            DB2_Communities()
+        return DB2_Communities.__instance
+    def __init__(self):
+        if DB2_Communities.__instance != None:
+            raise Exception("This class is a singleton")
+        else:
+            DB2_Communities.__instance = self
+            self.username = "lfj47179"
+            self.password = "3hb62wh+7jtbb77n"
+            self.host = "dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net"
+            self.port = 50000
+            self.schema = "BLUDB"#"LFJ47179"
+            self.db2ConnectionString = "db2+ibm_db://{}:{}@{}:{}/{}".format(self.username, self.password, self.host, self.port, self.schema)
+            self.engine = create_engine(self.db2ConnectionString,pool_size=1, max_overflow=0)
+            self.mainConnection = self.engine.connect()
+            self.metadata = MetaData()
+            self.Communities = Table('COMMUNITIES', self.metadata,
+                                    Column('POBLAC_ID', Integer(), nullable=False),
+                                    Column('PUEBLO', Text(), nullable=False),
+                                    Column('RESILIENCIA', Text(), nullable=False)
+                                    )
+
+
+    def insert_Community(self, CommunityDataAsDictionary):
+        if (self.is_community_in_database(CommunityDataAsDictionary)==False):
+            insert = self.Communities.insert().values(
+                POBLAC_ID = CommunityDataAsDictionary["POBLAC_ID"],
+                PUEBLO = CommunityDataAsDictionary["PUEBLO"],
+                RESILIENCIA = str(CommunityDataAsDictionary["RESILIENCIA"])
+            )
+            insert.compile().params
+            result = self.mainConnection.execute(insert)
+            insertMessage = "Community added to database."
+        else:
+            self.update_Community(CommunityDataAsDictionary)
+            insertMessage = "Community in database updated."
+        return insertMessage
+
+
+    def is_community_in_database(self, CommunityDataAsDictionary):
+        isCommunityInDB = False
+        selectQuery = select([self.Communities]).where(
+                                                       and_(
+                                                            self.Communities.c.POBLAC_ID==CommunityDataAsDictionary["POBLAC_ID"],
+                                                            self.Communities.c.PUEBLO==CommunityDataAsDictionary["PUEBLO"]
+                                                            )
+                                                       )
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        community = resultsProxy.fetchone()
+        if (community!=None):
+            isCommunityInDB = True
+        return isCommunityInDB
+
+    def update_Community(self, CommunityDataAsDictionary):
+        update = self.Communities.update().where(
+                                                 and_(
+                                                      self.Communities.c.POBLAC_ID==CommunityDataAsDictionary["POBLAC_ID"],
+                                                      self.Communities.c.PUEBLO==CommunityDataAsDictionary["PUEBLO"]
+                                                      )
+                                                 ).values(
+                                                          POBLAC_ID = CommunityDataAsDictionary["POBLAC_ID"],
+                                                          PUEBLO = CommunityDataAsDictionary["PUEBLO"]
+                                                          )
+        update.compile().params
+        result = self.mainConnection.execute(update)
+
+
+    def set_Community_Resilience(self, CommunityDataAsDictionary):
+        update = self.Communities.update().where(
+                                                 and_(
+                                                      self.Communities.c.POBLAC_ID==Poblac_ID,
+                                                      self.Communities.c.PUEBLO==Town
+                                                      )
+                                                 ).values(
+                                                          RESILIENCIA = CommunityDataAsDictionary["RESILIENCIA"]
+                                                          )
+        update.compile().params
+        result = self.mainConnection.execute(update)
+
+
+    def select_community_by_Poblac_ID(self, Poblac_ID):
+        selectQuery = select([self.Communities]).where(self.Communities.c.POBLAC_ID==Poblac_ID)
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        community = resultsProxy.fetchone()
+        resultsProxy.close()
+        return community #tuple
+
+    def select_community_Dictionary_by_Poblac_ID(self, Poblac_ID):
+        selectQuery = select([self.Communities]).where(self.Communities.c.POBLAC_ID==Poblac_ID)
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        Community = resultsProxy.fetchone()
+        CommunityAsDictionary = dict(
+            POBLAC_ID=Community[0],
+            PUEBLO=Community[1],
+            RESILIENCIA=Community[2]
+            )
+        resultsProxy.close()
+        return CommunityAsDictionary
+
+    def select_community_dictionary_by_state_city_and_name(self, State, City, CommunityName):
+        TownsDB = DB2_Towns.getInstance()
+        Town = TownsDB.select_Town_dictionary_by_State_City_and_Name(State, City, CommunityName)
+        Community = self.select_community_Dictionary_by_Poblac_ID(Town['POBLAC_ID'])
+        return Community
+
+
+class DB2_Resilience_Steps:
+    __instance = None
+    @staticmethod
+    def getInstance():
+        if DB2_Resilience_Steps.__instance == None:
+            DB2_Resilience_Steps()
+        return DB2_Resilience_Steps.__instance
+    def __init__(self):
+        if DB2_Resilience_Steps.__instance != None:
+            raise Exception("This class is a singleton")
+        else:
+            DB2_Resilience_Steps.__instance = self
+            self.username = "lfj47179"
+            self.password = "3hb62wh+7jtbb77n"
+            self.host = "dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net"
+            self.port = 50000
+            self.schema = "BLUDB"#"LFJ47179"
+            self.db2ConnectionString = "db2+ibm_db://{}:{}@{}:{}/{}".format(self.username, self.password, self.host, self.port, self.schema)
+            self.engine = create_engine(self.db2ConnectionString,pool_size=1, max_overflow=0)
+            self.mainConnection = self.engine.connect()
+            self.metadata = MetaData()
+            self.Resiliencia = Table('RESILIENCIA-PASOS', self.metadata,
+                                    Column('ID', Integer(), nullable=False, unique=True, primary_key=True),
+                                    Column('Etapa', Integer(), nullable=False),
+                                    Column('Paso', Integer(), nullable=False),
+                                    Column('Detalle', Text(), nullable=False),
+                                    Column('Referencia', Text(), nullable=True),
+                                    Column('Logros', Text(), nullable=True),
+                                    )
+
+
+    def select_all_Resilience_steps(self):
+        selectQuery = select([self.Resiliencia])
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        Resilience_steps = resultsProxy.fetchall()
+        #Clean list to get rid of Stage id, titles, references, achievments
+        Resilience_steps = [[step[1],step[2],step[3]] for step in Resilience_steps if step[2]!=0] #Stage,step,detail
+        resultsProxy.close()
+        return Resilience_steps
+
+
+    def select_all_Stages(self):
+        #Query only registers where Step equals zero --> step zero equals stage title
+        selectQuery = select([self.Resiliencia]).where(self.Resiliencia.c.Paso==0)
+        resultsProxy = self.mainConnection.execute(selectQuery)
+        Stages = resultsProxy.fetchall()
+        Stages = [[stage[1],stage[3]] for stage in Stages] #stage number, stage name
+        resultsProxy.close()
+        return Stages
 
 
 def main():
-    db = DB2()
-    data= dict(
-        NAME = "Upala",
-        UBICATION_X = -83.123,
-        UBICATION_Y = 10.522,
-        PROVINCE = "Alajuela",
-        CANTON = "Upala",
-        RESILIENCE='{"badges":[{"icon": "url_for_image","title": "Beginner","description": "First Badge"},{"icon": "url_for_image","title": "Second Runner","description": "Second Badge"},{"icon": "url_for_image","title": "Third Runner","description": "Third Badge"}],"resilience_level": 0.8}'
-        )
-    #db.insert(data)
-    communities = db.select()
-    print(communities)
+    dbp = DB2_Resilience_Steps.getInstance()
+    stages = dbp.select_all_Stages()
+    resiliencia = dbp.select_all_Resilience_steps()
+    pprint(stages)
+    pprint(resiliencia)
+    #db = DB2_Communities()
+    #db.insert_Community(town)
+    #comunidad = db.select_community_dictionary_by_state_city_and_name("HEREDIA","SAN RAFAEL","CONCEPCION")
+    #print(comunidad)
 
 if __name__=="__main__":
     main()
--- resilapp.py
+++ resilapp.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, jsonify
 import atexit
 import os
 import json
+from SQL_Database import DB2_Towns, DB2_Communities
 
 app = Flask(__name__, static_url_path='')
 
@@ -51,67 +52,127 @@ def root():
 #  * </code>
 #  * @return An array of all the communities data
 #  */
-@app.route('/api/communities', methods=['GET'])
-def get_communities():
-    if client:
-        return jsonify(list(map(lambda doc: doc, db)))
-    else:
-        print('No database')
-        return jsonify([])
-
-@app.route('/api/communities/<community>', methods=['GET'])
-def get_community(community):
-    if client:
-    	community_list = list(map(lambda doc:(doc if doc['name']==community else None), db))
-    	community_list = [i for i in community_list if i!=None]
-        return jsonify(community_list)
-    else:
-        print('No database')
-        return jsonify([])
 
 # /* Endpoint to add a new community to database.
 # * Send a POST request to localhost:8000/api/visitors with body
 # * {
 # *     "name": "Community Name",
-#		"ubication_x": Longitude_As_Number,
-#		"ubication_y": Latitude_As_Number,
-#		"province": Province or State,
-#		"canton": City,
-#		"resilience": {Resilience_Object}
+#       "ubication_x": Longitude_As_Number,
+#       "ubication_y": Latitude_As_Number,
+#       "province": Province or State,
+#       "city": City,
+#       "resilience": {Resilience_Object}
 # * }
 # */
 # /* A Resilience_Object will contain:
-#	{
-#		"resilience_level":Decimal_Number_From_Zero_To_One,
-#		"badges":[{				------> This one is a list, containing one or several ResilienceBadge_Object(s)
-#			"title":"Beginner",
-#			"description":"First Badge",
-#			"icon":"url_for_image"
-#	}
+#   {
+#       "resilience_level":Decimal_Number_From_Zero_To_One,
+#       "badges":[{             ------> This one is a list, containing one or several ResilienceBadge_Object(s)
+#           "title":"Beginner",
+#           "description":"First Badge",
+#           "icon":"url_for_image"
+#   }
 #*/
-@app.route('/api/communities', methods=['POST'])
-def put_community():
-    name = request.json['name']
-    ubication_x = request.json['ubication_x']
-    ubication_y = request.json['ubication_y']
-    province = request.json['province']
-    canton = request.json['canton']
-    resilience = request.json['resilience']
-    data = {
-    		'name':name,
-    		'ubication_x':ubication_x,
-			'ubication_y':ubication_y,
-			'province':province,
-			'canton':canton,
-			'resilience':resilience
-    		}
+@app.route('/api/communities', methods=['POST', 'GET'])
+def communities():
+    if request.method == 'GET':
+        if client:
+	        db = "resilapp-communities"
+            return jsonify(list(map(lambda doc: doc, db)))
+        else:
+            print('No database')
+            return jsonify([])
+    elif request.method == 'POST':
+        name = request.json['name']
+        ubication_x = request.json['ubication_x']
+        ubication_y = request.json['ubication_y']
+        province = request.json['province']
+        city = request.json['city']
+        resilience = request.json['resilience']
+        data = {
+                'name':name,
+                'ubication_x':ubication_x,
+                'ubication_y':ubication_y,
+                'province':province,
+                'city':city,
+                'resilience':resilience
+                }
+        if client:
+        	db = "resilapp-communities"
+            my_document = db.create_document(data)
+            data['_id'] = my_document['_id']
+            return jsonify(data)
+        else:
+            print('No database')
+            return jsonify(data)
+    
+
+@app.route('/api/communities/<community>', methods=['GET'])
+def get_community(community):
     if client:
+    	db = "resilapp-communities"
+        community_list = list(map(lambda doc:(doc if doc['name']==community else None), db))
+        community_list = [i for i in community_list if i!=None]
+        return jsonify(community_list)
+    else:
+        print('No database')
+        return jsonify([])
+
+
+@app.route('/api/communities/<province>/<city>/<town>', methods=['POST'])
+def put_community(province, city, town):
+    db_towns = DB2_Towns.getInstance()
+    db_communities = DB2_Communities.getInstance()
+    town = db_towns.select_Town_dictionary_by_State_City_and_Name(province, city, town)
+    POBLAC_ID = town["POBLAC_ID"]
+    PUEBLO = "{},{},{}".format(town["PUEBLO"],town["CANTON"],town["PROVINCIA"])
+    RESILIENCIA = request.json['RESILIENCIA']
+    data = {
+            'POBLAC_ID':POBLAC_ID,
+            'PUEBLO':PUEBLO,
+            'RESILIENCIA':RESILIENCIA
+            }
+    if client:
+        db_name = 'resilapp-communities-badges'
+        db = client.create_database(db_name, throw_on_exists=False)
         my_document = db.create_document(data)
         data['_id'] = my_document['_id']
+        community = list(map(lambda doc:(doc if doc['PUEBLO']==PUEBLO else None), db))
+        community = [i for i in community if i!=None]
+        community = community[0]
+        town['RESILIENCIA'] = dict(_id=community['_id'],_rev=community['_rev'])
+        db_communities.insert_Community(town)
         return jsonify(data)
     else:
         print('No database')
         return jsonify(data)
+
+
+@app.route('/api/towns/provinces', methods=['GET'])
+def get_provinces():
+    db = DB2_Towns.getInstance()
+    provinces = db.select_all_States()
+    return jsonify(provinces)
+
+@app.route('/api/towns/<province>', methods=['GET'])
+def get_cities(province):
+    db = DB2_Towns.getInstance()
+    cities = db.select_all_Cityes_by_State(province)
+    return jsonify(cities)
+
+@app.route('/api/towns/<province>/<city>', methods=['GET'])
+def get_towns(province,city):
+    db = DB2_Towns.getInstance()
+    towns = db.select_all_Towns_by_State_and_City(province, city)
+    return jsonify(towns)
+
+
+@app.route('/api/towns/<province>/<city>/<town>', methods=['GET'])
+def get_poblado(province,city,town):
+    db = DB2_Towns.getInstance()
+    town = db.select_Town_dictionary_by_State_City_and_Name(province, city, town)
+    return jsonify(town)
+
 
 @atexit.register
 def shutdown():
